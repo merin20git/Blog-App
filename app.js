7ab60e65926a919686565fdff95a9dda9030fb3f
merin20git <merin20122003@gmail.com>
2025-09-01 23:41:30 +0530

    new

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
 const userModel=require("./models/users")
+const postModel = require("./models/posts")
 
 let app = Express() 
 
@@ -11,6 +12,27 @@ app.use(Express.json())
 app.use(Cors())
 
 Mongoose.connect("mongodb+srv://merin20122003:G9bPwozwvMjns1k9@cluster0.wyrfz.mongodb.net/blogAppDb?retryWrites=true&w=majority&appName=Cluster0")
+
+//create a post
+
+app.post("/create",async(req,res)=>{
+    let input=req.body
+
+    let token=req.headers.token
+
+    jwt.verify(token,"blogApp",async(error,decoded)=>{
+        if (decoded && decoded.email) {
+            
+            let result=new postModel(input)
+            await result.save()
+            res.json({"status":"success"})
+
+        } else {
+            res.json({"status":"Invalid Authentication"})
+        }
+    })
+
+})
 
 //Sign In
 app.post("/signIn",async(req,res)=>{
